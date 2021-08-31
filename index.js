253254459db8123191bcc04caeb21626a01e3ac9
Dhekra Nsir <dhekransir@gmail.com>
2021-08-31 09:29:07 +0100

    express2

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require ('express')
 const app = express();
 const port = 4000
-// app.get("/",(req,res)=>{
-//     res.send("home");
-// })
-app.use(express.static("public"))
+
+app.use(express.static("./public"))
+
+  app.use(function (req, res, next) {
+    let date= new Date()
+    console.log(date)
+    let day=date.getDay()
+    console.log(day)
+    let hour=date.getHours()
+    console.log(hour)
+    if ((day !== 0 && day !== 6 )&& (hour <= 17 && hour >= 10)) {
+        next();
+    } 
+    
+    else {
+        res.send('<h1>Out of service Now<h1>')
+    }
+
+})
+  app.get('/', function (req, res,next) {
+    res.send('Welcome Home');
+  });
+
+
 app.listen(4000,(err)=>{
     err?console.log(err): console.log('server is runing')
 })
