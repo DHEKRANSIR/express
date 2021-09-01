b6596f1d30681203a5de7e66c3989cd4b2d12faa
Dhekra Nsir <dhekransir@gmail.com>
2021-09-01 09:55:03 +0100

    exp final

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express();
 const port = 4000
 
-app.use(express.static("./public"))
+
 
   app.use(function (req, res, next) {
     let date= new Date()
